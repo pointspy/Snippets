712bf729561ac1ada8941b278f1b6c5f3245f49d
Pavel Lyskov <pavellyskov_bks@192.168.0.9>
2022-04-30 03:59:31 +0500

    First commit

---
+++ Property Wrappers/UserDefaultsWrapper.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+@propertyWrapper struct UserDefaultsWrapper<T> {
+	let key: String
+	let defaultValue: T
+	
+	init(_ key: String, defaultValue: T) {
+		self.key = key
+		self.defaultValue = defaultValue
+	}
+	
+	var wrappedValue: T {
+		get {
+			return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+		}
+		set {
+			if let value = newValue as? OptionalProtocol, value.isNil() {
+				UserDefaults.standard.removeObject(forKey: key)
+			} else {
+				UserDefaults.standard.set(newValue, forKey: key)
+			}
+		}
+	}
+}
+ 
+private protocol OptionalProtocol {
+	func isNil() -> Bool
+}
+
+extension Optional: OptionalProtocol {
+	func isNil() -> Bool {
+		return self == nil
+	}
+}
---
+++ Property Wrappers/atomic.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+@propertyWrapper
+final class Atomic<Value> {
+    private var lock: NSRecursiveLock = NSRecursiveLock()
+
+    private var value: Value
+
+    var wrappedValue: Value {
+        get {
+            lock.lock(); defer { lock.unlock() }
+            return value
+        }
+
+        set {
+            lock.lock(); defer { lock.unlock() }
+            value = newValue
+        }
+    }
+
+    init(wrappedValue value: Value) {
+        self.value = value
+    }
+}
