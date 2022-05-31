1b54e4ca1b2f30980ec1b9ba22dbe0f075dabb46
pointspy <pointspy@me.com>
2022-05-31 20:16:43 +0500

    pop/push nvc

---
+++ ViewControllers/UINavigationController+.swift
@@ -0,0 +1,32 @@
+import UIkit
+
+extension UINavigationController {
+    public func pushViewController(
+        _ viewController: UIViewController,
+        animated: Bool,
+        completion: @escaping () -> Void)
+    {
+        pushViewController(viewController, animated: animated)
+
+        guard animated, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return
+        }
+
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
+
+    func popViewController(
+        animated: Bool,
+        completion: @escaping () -> Void)
+    {
+        popViewController(animated: animated)
+
+        guard animated, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return
+        }
+
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
+}
